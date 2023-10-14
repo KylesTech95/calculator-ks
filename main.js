df3ff8afbfec61f679748feb82e62893c17cebad
kylestech95 <kylesTech95@outlook.com>
2023-10-13 22:59:07 -0400

    updated with flex-box & @media query

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 let btns = document.querySelectorAll('.btn'),
-display = document.querySelector('.display-actual'),
-board = document.querySelector('.board');
+    display = document.querySelector('.display-actual'),
+    board = document.querySelector('.board');
 
-//helper function to determine if there is a decimal in the current
-const oneDecimal = (str) =>{
-    let hasOne = (/\.{1}/).test(str)
-    return !hasOne
-}
 
 //Looping throught the btns array
 btns.forEach(btn => {
@@ -18,30 +13,38 @@ btns.forEach(btn => {
         if(/\./.test(chr) && oneDecimal(display.value)){
             display.value += chr;
         }
-            //Regex.test(string) - condition for programming chrs, deleting chrs & All Clear(AC)
+        //Regex.test(string) - condition for programming chrs, deleting chrs & All Clear(AC)
         if(/\d|[0-9-+*/]/.test(chr)){
             display.value += chr;
         }
-        if(/DELETE/.test(chr))display.value = display.value.slice(0,-1)
+        if(/(\+|\-|\/|\*)(\+|\-|\/|\*)/g.test(display.value)){
+            display.value = display.value.replace(/(\+|\-|\/|\*)/,'')
+        }
+        if(/DELETE/.test(chr))display.value = display.value.slice(0,-1);
+
         if(/AC/.test(chr)){
             display.value = '0';
             board.textContent='0'}
+            
         //Equals(Evaluation)
         if(/=/.test(chr)){
             //Helper function to evaluate
             display.value = eval(display.value);
-            board.innerHTML = eval(display.value);}
+            board.innerHTML = eval(display.value);
+        }
     })
 })
-
 //Event listener to detect keys
 window.addEventListener('keydown',(e)=>{
     if(display.value=='0')display.value=''
-if(/\./.test(e.key) && oneDecimal(display.value)){
+if(/\./.test(e.key)){
     display.value += e.key;
 }
     if(/[0-9-+*/]/.test(e.key)){
         display.value += e.key;
+    }
+    if(/(\+|\-|\/|\*)(\+|\-|\/|\*)/g.test(display.value)){
+        display.value = display.value.replace(/(\+|\-|\/|\*)/,'')
     }
     if(e.key=='Enter'|| e.key=='='){
     board.innerHTML= eval(display.value);
